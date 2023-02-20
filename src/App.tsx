fb1548351640834c415ebcc494772a8ebc002c53
William Knight <William.Jeffery.Knight@protonmail.com>
2023-02-20 06:48:05 -0500

    basic setup directory structure and install basic packets

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App(){
+
+  return (
+    <h1>Hi</h1>
+  )
+  }
+export default App
+// How To Create An Advanced Shopping Cart With React and TypeScript
+// follow Web Dev Simplfified https://youtu.be/lATafp15HWA
